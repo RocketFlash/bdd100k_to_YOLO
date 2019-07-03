4122cfa182c2487df55e1f2cdb8324783343c9dc
Rauf <goodraff@gmail.com>
2019-07-03 20:27:22 +0000

    modify functionality

--- bdd_to_yolo_converter.py
+++ bdd_to_yolo_converter.py
@@ -1,21 +1,24 @@
 import json
 import os
 from tqdm import tqdm
+from config import PATHS, categories, img_size
 
 
-def generate_yolo_labels(json_path, save_path, categories, img_size, is_train=True):
+def generate_yolo_labels(json_path, save_path, fname_prefix=None, fname_postfix=None):
     img_w, img_h = img_size
     ignore_categories = ["drivable area", "lane"]
+    print('start YOLO labels creation')
     with open(json_path) as json_file:
         data = json.load(json_file)
-        print('Total number of files: {}'.format(len(data)))
         for img in tqdm(data):
-            img_name = img['name']
-            img_label_txt = str(img_name[:-4]) + ".txt"
+            img_name = str(img['name'][:-4])
+            img_label_txt = (
+                fname_prefix if fname_prefix is not None else '') + img_name
+            img_label_txt += (
+                fname_postfix if fname_postfix is not None else '') + ".txt"
             img_labels = [l for l in img['labels']
                           if l['category'] not in ignore_categories]
-            file_path = save_path + 'original_day/' + \
-                ('train_' if is_train else 'val_') + img_label_txt
+            file_path = save_path + img_label_txt
             with open(file_path, 'w+') as f_label:
                 for label in img_labels:
                     y1 = label['box2d']['y1']
@@ -42,51 +45,57 @@ def generate_yolo_labels(json_path, save_path, categories, img_size, is_train=Tr
                     f_label.write(line_to_write + "\n")
 
 
-def generate_yolo_filenames(imgs_path, output_file_path, owner_prefix=None, img_start_prefix=None, exclude=[]):
-    print('Start YOLO filenames file creation')
-    if owner_prefix is None:
-        owner_prefix = '/home/'+os.getlogin()+'/'
+def generate_yolo_filenames(imgs_path, output_file_path, fname_prefix=None, fname_postfix=None):
+    print('start YOLO filenames file creation')
+    print(output_file_path)
     with open(output_file_path, 'w+') as f:
         for dirpath, dirs, files in os.walk(imgs_path):
-            dirs[:] = [d for d in dirs if d not in exclude]
-            for filename in files:
-                file_path = os.path.join(owner_prefix, dirpath, filename)
+            for filename in tqdm(files):
+                file_path = os.path.join(
+                    PATHS['owner_prefix'], dirpath, filename)
                 if filename.endswith(".jpg"):
-                    if img_start_prefix is not None:
-                        if filename.startswith(img_start_prefix):
-                            f.write(file_path + "\n")
-                    else:
+                    fname_prefix = fname_prefix if fname_prefix is not None else ''
+                    fname_postfix = fname_postfix if fname_postfix is not None else ''
+                    if filename.startswith(fname_prefix) and filename.endswith(fname_postfix):
                         f.write(file_path + "\n")
 
 
-def generate_names_file(categories, filename='bdd100k.names'):
-    output_file_path = filename
+def generate_names_file(filename='bdd100k.names'):
+    output_file_path = PATHS['save_path'] + filename
     with open(output_file_path, 'w+') as f:
         for key, _ in categories.items():
             f.write(key + "\n")
+    print('{} created'.format(filename))
 
 
 def generate_data_file(filename='bdd100k.data'):
-    output_file_path = filename
+    output_file_path = PATHS['save_path'] + filename
     with open(output_file_path, 'w+') as f:
-        pass
+        f.write('classes = 10' + '\n')
+        f.write('train = {}train.txt'.format(PATHS['save_path']) + '\n')
+        f.write('valid = {}val.txt'.format(PATHS['save_path']) + '\n')
+        f.write('names = {}bdd100k.names'.format(PATHS['save_path']) + '\n')
+        f.write('backup = {}backup'.format(PATHS['save_path']) + '\n')
+    print('{} created'.format(filename))
 
 
 if __name__ == '__main__':
 
-    generate_yolo_labels(train_labels_json_path, save_path,
-                         categories, img_size, is_train=True, with_fake=True)
-    generate_yolo_labels(val_labels_json_path, save_path,
-                         categories, img_size, is_train=False, with_fake=True)
+    generate_yolo_labels(PATHS['train_labels_json_path'], PATHS['labels_save_path_train'],
+                         fname_prefix='train_', fname_postfix=None)
+    generate_yolo_labels(PATHS['val_labels_json_path'], PATHS['labels_save_path_val'],
+                         fname_prefix='val_', fname_postfix=None)
 
-    with_fake_night = False
-    save_path = dataset_path + 'yolo_files/' + \
-        ('with_fake/' if with_fake_night else 'without_fake/')
-    train_file_path = save_path + 'train.txt'
-    val_file_path = save_path + 'val.txt'
-    exclude = [] if with_fake_night else ['fake_night']
+    # generate labels for augmented images
+    generate_yolo_labels(PATHS['train_labels_json_path'], PATHS['labels_save_path_train'],
+                         fname_prefix='train_', fname_postfix='_fake_B')
+    generate_yolo_labels(PATHS['val_labels_json_path'], PATHS['labels_save_path_val'],
+                         fname_prefix='val_', fname_postfix='_fake_B')
 
-    generate_yolo_filenames(images_path, train_file_path,
-                            img_start_prefix='train_', exclude=exclude)
-    generate_yolo_filenames(images_path, val_file_path,
-                            img_start_prefix='val_', exclude=exclude)
+    generate_yolo_filenames(PATHS['images_path_train'], PATHS['train_file_path'],
+                            fname_prefix='train_', fname_postfix=None)
+    generate_yolo_filenames(PATHS['images_path_val'], PATHS['val_file_path'],
+                            fname_prefix='val_', fname_postfix=None)
+
+    generate_names_file(filename='bdd100k.names')
+    generate_data_file(filename='bdd100k.data')
